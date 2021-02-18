00ef166ecd85263c3793ea4bc5f6a6f9fc5576cb
gear5km <60645081+gear5km@users.noreply.github.com>
2021-02-18 14:14:41 +0300

    v0.31
    Added Decryptor Function

--- caesar_cipher.java
+++ caesar_cipher.java
@@ -26,7 +26,15 @@ class caesar_cipher {
         //typedCode(userInput);sssa
         int[]inputVals=stringToNumber(userMessage);
         int []encryptedVal=encryptor(inputVals, shiftValue);
-        System.out.println(encryptedVal[0]);
+        int []decryptedVal=decryptor(encryptedVal, shiftValue);
+        String [] outputVal= numberToString(decryptedVal);
+        //System.out.println(encryptedVal[0]);
+        //System.out.println(decryptedVal[0]);
+
+        String outputValDecoded= Arrays.toString(outputVal);
+
+        String str = String.join("", outputVal);
+        System.out.println("the value is " + str);
     }
     /*****************************************************************
      Assigns a Value to each of the Characters typed by the user
@@ -158,6 +166,127 @@ class caesar_cipher {
         encryptedVal= inputVals;
         System.out.println(Arrays.toString(encryptedVal));
         return encryptedVal;
+    }
+    static int[] decryptor(int encryptedVal [], int shiftValue ){
+        System.out.println(shiftValue);
+
+        System.out.println(encryptedVal.length);
+        int encryptedValLength = encryptedVal.length;
+        int [] decryptorVal={};
+
+        for (int i=0; i< encryptedValLength; i++){
+            System.out.println(encryptedVal[i]);
+        };
+
+        for (int i = 0; i < encryptedVal.length; i++){
+            encryptedVal[i]= encryptedVal[i]- shiftValue;
+        }
+        System.out.println("checking wait " + encryptedVal[0]);
+
+
+        decryptorVal= encryptedVal;
+        System.out.println(Arrays.toString(encryptedVal));
+        return decryptorVal;
+    }
+    static String [] numberToString (int decryptedVal[]) {
+
+        int[] outputConverter = decryptedVal;
+        System.out.println("input Line is "+ outputConverter.length + " Characters Long");
+
+        String [] outputVal;
+        outputVal= new String [outputConverter.length];
+
+        for (int i= 0; i < outputConverter.length; i++) {
+            System.out.println(outputConverter[i]);
+        }
+        //System.out.println(array[1]);
+
+        for (int a=0;a<outputConverter.length; a++){
+            switch (outputConverter[a]){
+                case 1:
+                    outputVal[a]="a";
+                    break;
+                case 2:
+                    outputVal[a]="b";
+                    break;
+                case 3:
+                    outputVal[a]="c";
+                    break;
+                case 4:
+                    outputVal[a]="d";
+                    break;
+                case 5:
+                    outputVal[a]="e";
+                    break;
+                case 6:
+                    outputVal[a]="f";
+                    break;
+                case 7:
+                    outputVal[a]="g";
+                    break;
+                case 8:
+                    outputVal[a]="h";
+                    break;
+                case 9:
+                    outputVal[a]="i";
+                    break;
+                case 10:
+                    outputVal[a]="j";
+                    break;
+                case 11:
+                    outputVal[a]="k";
+                    break;
+                case 12:
+                    outputVal[a]="l";
+                    break;
+                case 13:
+                    outputVal[a]="m";
+                    break;
+                case 14:
+                    outputVal[a]="n";
+                    break;
+                case 15:
+                    outputVal[a]="o";
+                    break;
+                case 16:
+                    outputVal[a]="p";
+                    break;
+                case 17:
+                    outputVal[a]="q";
+                    break;
+                case 18:
+                    outputVal[a]="r";
+                    break;
+                case 19:
+                    outputVal[a]="s";
+                    break;
+                case 20:
+                    outputVal[a]="t";
+                    break;
+                case 21:
+                    outputVal[a]="u";
+                    break;
+                case 22:
+                    outputVal[a]="v";
+                    break;
+                case 23:
+                    outputVal[a]="w";
+                    break;
+                case 24:
+                    outputVal[a]="x";
+                    break;
+                case 25:
+                    outputVal[a]="y";
+                    break;
+                case 26:
+                    outputVal[a]="z";
+                    break;
+            }
+        }
+        System.out.println(Arrays.toString(outputVal));
+
+
+        return outputVal;
     }
 
 
